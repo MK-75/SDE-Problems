27f4ec43fc47d37d120c5dca7ba8d90066b48195
MK-75 <mkparekh2803@gmail.com>
2022-08-13 09:25:48 +0530

    Array-II: Rotate Matrix

---
+++ Day2-Arrays/rotate_matrix.cpp
@@ -0,0 +1,62 @@
+/*
+ *
+ *
+Problem Statement:
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+ *
+ *
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++)
+                swap(matrix[i][j], matrix[j][i]);
+        }
+        
+        n = n - 1;
+        for(int i=0; i<=n; i++) {
+            for(int j=0; j<=n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j];
+                matrix[i][n-j] = temp;
+            }
+        }
+    }
+};
+
+int main() {
+    int N, M;
+    cin >> M >> N;
+
+    vector<vector<int>> matrix(M, vector<int>(N));
+
+    for(int i=0; i<M; i++) {
+        for(int j=0; j<N; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    Solution solution;
+    solution.rotate(matrix);
+
+    for(int i=0; i<M; i++) {
+        for(int j=0; j<N; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
